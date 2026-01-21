d95860f8c54aee265f3a0f6ecb11a93c2f91e0cd
Lucas <lucagabrielsantos5@gmail.com>
2026-01-21 10:14:10 -0300

    setting & .gitingore updated

--- project/settings.py
+++ project/settings.py
@@ -121,5 +121,7 @@ STATIC_URL = 'static/'
 STATICFILES_DIRS = (
     BASE_DIR / 'base_static',
 )
-
-  
+STATIC_ROOT = BASE_DIR / 'static'
+MEDIA_URL = 'media/'
+MEDIA_ROOT = BASE_DIR / 'media'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
