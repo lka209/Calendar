5b1e6d1adfe9c18870ccb61ecd45b1fa727f772a
Lucas <lucagabrielsantos5@gmail.com>
2026-01-21 08:51:40 -0300

    settings.jason update

--- contact/admin.py
+++ contact/admin.py
@@ -5,5 +5,8 @@ from contact import models
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
     list_display = ('first_name', 'last_name', 'email', 'created_date')
-    search_fields = ('first_name', 'last_name', 'email', 'category')
+    list_editable = ('first_name', 'last_name', 'email', 'category')
     list_filter = ('category', 'created_date', 'first_name', 'last_name')
+    search_fields = ('first_name', 'last_name', 'email', 'category')
+    list_per_page = 10
+    list_max_show_all = 200
