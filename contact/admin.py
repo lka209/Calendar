e6bbe5da7f4fbd844c9af7a3e89abeb378ea1fe5
Lucas <lucagabrielsantos5@gmail.com>
2026-01-21 10:48:14 -0300

    admin/migrations

--- contact/admin.py
+++ contact/admin.py
@@ -4,9 +4,10 @@ from contact import models
 
 @admin.register(models.Contact)
 class ContactAdmin(admin.ModelAdmin):
-    list_display = ('first_name', 'last_name', 'email', 'created_date')
-    list_editable = ('first_name', 'last_name', 'email', 'category')
+    list_display = ('first_name', 'last_name', 'email', 'created_date', 'show', 'category')
+    list_editable = ('first_name', 'last_name', 'email', 'category', 'show')
     list_filter = ('category', 'created_date', 'first_name', 'last_name')
+    list_display_links = None
     search_fields = ('first_name', 'last_name', 'email', 'category')
     list_per_page = 10
     list_max_show_all = 200
