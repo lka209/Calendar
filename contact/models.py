e51b19342367c19e72f1b46cbeefdcfc6fe4f8a6
Lucas <lucagabrielsantos5@gmail.com>
2026-01-23 10:01:36 -0300

    models updt

--- contact/models.py
+++ contact/models.py
@@ -1,25 +1,18 @@
 from django.db import models
 from django.utils import timezone
 
-first_name = models.CharField(max_length=30)
-last_name = models.CharField(max_length=30)
-email = models.EmailField(max_length=254, blank=True)
-category = models.CharField(max_length=50)
-created_date = models.DateTimeField(default=timezone.now)
-description = models.TextField(blank=True)
-show = models.BooleanField(default=True)
-picture = models.ImageField(upload_to='pictures/%Y/%m/', blank=True)
+class Category(models.Model):
+    name = models.CharField(max_length=100)
 
+class Contact(models.Model):
+    first_name = models.CharField(max_length=30)
+    last_name = models.CharField(max_length=30)
+    email = models.EmailField(max_length=254, blank=True)
+    category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
+    created_date = models.DateTimeField(default=timezone.now)
+    description = models.TextField(blank=True)
+    show = models.BooleanField(default=True)
+    picture = models.ImageField(upload_to='pictures/%Y/%m/', blank=True)
 
 def __str__(self) -> str:
     return f"{self.first_name} {self.last_name}"
-
-class Contact(models.Model):
-    first_name = first_name
-    last_name = last_name
-    email = email
-    category = category
-    created_date = created_date
-    description = description
-    picture = picture
-    show = show
