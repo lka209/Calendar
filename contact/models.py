2ab01daa9900f6beb31f31ce46c9bcda23977640
Lucas <lucagabrielsantos5@gmail.com>
2026-01-26 08:48:56 -0300

    updt

--- contact/models.py
+++ contact/models.py
@@ -2,6 +2,10 @@ from django.db import models
 from django.utils import timezone
 
 class Category(models.Model):
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        
     name = models.CharField(max_length=100)
     
     def __str__(self) -> str:
@@ -11,13 +15,14 @@ class Contact(models.Model):
     first_name = models.CharField(max_length=30)
     last_name = models.CharField(max_length=30)
     email = models.EmailField(max_length=254, blank=True)
+    phone = models.CharField(max_length=15, blank=True)
     category = models.ForeignKey(Category, on_delete=models.SET_NULL, null=True, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
     picture = models.ImageField(upload_to='pictures/%Y/%m/', blank=True)
-    phone = models.CharField(max_length=15, blank=True)
+    owner = models.ForeignKey('auth.User', on_delete=models.SET_NULL, null=True, blank=True)
     
-
-def __str__(self) -> str:
-    return f"{self.first_name} {self.last_name}"
+    
+    def __str__(self) -> str:
+        return f"{self.first_name} {self.last_name}"
