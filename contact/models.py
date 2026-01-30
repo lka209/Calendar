f86ebefc096cfb9552f9451ca13de64bdbcfe863
Lucas <lucagabrielsantos5@gmail.com>
2026-01-30 09:49:53 -0300

    updt

--- contact/models.py
+++ contact/models.py
@@ -23,10 +23,10 @@ class Category(models.Model):
 
 
 class Contact(models.Model):
-    first_name = models.CharField(max_length=50)
-    last_name = models.CharField(max_length=50, blank=True)
-    phone = models.CharField(max_length=50)
-    email = models.EmailField(max_length=254, blank=True)
+    first_name = models.CharField(verbose_name='First Name', max_length=50)
+    last_name = models.CharField(verbose_name='Last Name', max_length=50, blank=True)
+    phone = models.CharField(verbose_name='Phone', max_length=50)
+    email = models.EmailField(verbose_name='Email', max_length=254, blank=True)
     created_date = models.DateTimeField(default=timezone.now)
     description = models.TextField(blank=True)
     show = models.BooleanField(default=True)
--- contact/urls.py
+++ contact/urls.py
@@ -1,6 +1,5 @@
 # type:ignore
 from django.urls import path
-
 from contact import views
 
 app_name = 'contact'
