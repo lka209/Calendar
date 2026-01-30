1a010eca45f8433a5a048f314a448244656688d6
Lucas <lucagabrielsantos5@gmail.com>
2026-01-30 08:09:11 -0300

    updt

--- contact/views/contact_forms.py
+++ contact/views/contact_forms.py
@@ -6,12 +6,24 @@ from contact.models import Contact
 
 
 def create(request):
+    post = request.POST
+    if request.method == 'POST':
+        print()
+        print(request.method)
+        print(request.POST.get('first_name'))
+        print(request.POST.get('last_name'))
+        print()
+
     context = {
 
-    }
+}
 
-    return render(
-        request,
-        'contact/create.html',
-        context
-    )
+    print()
+    print(request.method)
+    print()
+
+return render(
+request,
+'contact/create.html',
+context
+)
