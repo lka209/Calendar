0a71adde271ad7fda00d6e4abd6b8ceae3addddf
Lucas <lucagabrielsantos5@gmail.com>
2026-02-02 10:10:13 -0300

    updt

--- contact/urls.py
+++ contact/urls.py
@@ -1,10 +1,17 @@
+# type:ignore
 from django.urls import path
-from . import views
+
+from contact import views
 
 app_name = 'contact'
 
 urlpatterns = [
-    path('create/', views.create, name='create'),
-    path('<int:contact_id>/', views.contact, name='contact'),
-    path('<int:contact_id>/update/', views.update, name='update'),
+path('', views.index, name='index'),
+path('search/', views.search, name='search'),
+
+# contact (CRUD)
+path('contact/<int:contact_id>/', views.contact, name='contact'),
+path('contact/create/', views.create, name='create'),
+path('contact/<int:contact_id>/update/', views.update, name='update'),
+    path('contact/<int:contact_id>/delete/', views.delete, name='delete'),
 ]
