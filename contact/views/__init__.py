f8dce293200cb6ae79aa65fccf15a7c611474d18
Lucas <lucagabrielsantos5@gmail.com>
2026-02-03 09:06:06 -0300

    updt

--- contact/views/__init__.py
+++ contact/views/__init__.py
@@ -1,3 +1,15 @@
 # flake8: noqa
 # type: ignore
 from .contact_views import *
+def create(request):
+    pass
+def update(request, contact_id):
+    pass
+def delete(request, contact_id):
+    pass
+def register(request):
+    pass
+def login_view(request):
+    pass
+def logout_view(request):
+    pass
