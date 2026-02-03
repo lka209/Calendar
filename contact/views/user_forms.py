401371a13ac4c202f1f486b68f8fcae9f0eb4607
Lucas <lucagabrielsantos5@gmail.com>
2026-02-03 08:35:42 -0300

    updt

--- contact/views/user_forms.py
+++ contact/views/user_forms.py
@@ -1,56 +1,30 @@
+from django.contrib import messages
 from django.contrib import auth, messages
-from django.contrib.auth.decorators import login_required
 from django.contrib.auth.forms import AuthenticationForm
 from django.shortcuts import redirect, render
-from contact.forms import RegisterForm, RegisterUpdateForm
+
+from contact.forms import RegisterForm
 
 
 def register(request):
-    form = RegisterForm()
+ form = RegisterForm()
 
-    if request.method == 'POST':
-        form = RegisterForm(request.POST)
+if request.method == 'POST':
+ form = RegisterForm(request.POST)
 
-        if form.is_valid():
-            form.save()
-            messages.success(request, 'Usuário registrado')
-            return redirect('contact:login')
+if form.is_valid():
+ form.save()
+messages.success(request, 'Usuário registrado')
+return redirect('contact:index')
+return redirect('contact:login')
 
-    return render(
-        request,
-        'contact/register.html',
-        {
-            'form': form
-        }
-    )
-
-
-@login_required(login_url='contact:login')
-def user_update(request):
-    form = RegisterUpdateForm(instance=request.user)
-
-    if request.method != 'POST':
-        return render(
-            request,
-            'contact/user_update.html',
-            {
-                'form': form
-            }
-        )
-
-    form = RegisterUpdateForm(data=request.POST, instance=request.user)
-
-    if not form.is_valid():
-        return render(
-            request,
-            'contact/user_update.html',
-            {
-                'form': form
-            }
-        )
-
-    form.save()
-    return redirect('contact:user_update')
+return render(
+request,
+'contact/register.html',
+{
+'form': form
+}
+)
 
 
 def login_view(request):
@@ -75,7 +49,6 @@ def login_view(request):
     )
 
 
-@login_required(login_url='contact:login')
 def logout_view(request):
     auth.logout(request)
     return redirect('contact:login')
